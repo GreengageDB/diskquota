303cea1161f457575bc2a8c7eddaa18a64b8eb13
Viktor Kurilko <v.kurilko@arenadata.io>
2025-12-17 20:30:30 +0700

    review based fix

--- ci/test_in_docker.bash
+++ ci/test_in_docker.bash
@@ -2,8 +2,8 @@
 
 set -xeu
 
-source /usr/local/greengage-db-devel/greengage_path.sh;
-source /home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh;
+source /usr/local/greengage-db-devel/greengage_path.sh
+source /home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 
 pushd /home/gpadmin/gpdb_src
   make -C src/test/isolation2 install
@@ -15,31 +15,20 @@ cmake ..
 cmake --build .
 make install
 
-if ! gpinitstandby -ar; then
-  echo "failed to disable standby"
-  exit 1
-fi
+gpinitstandby -ar;
 
 export SHOW_REGRESS_DIFF=1
+
 # test without standby
-set +e
-cmake --build . --target installcheck
-errors=$?
-set -e
+cmake --build . --target installcheck || errors=$?
 
 cp tests/regress/regression.diffs /logs/regression_regress_with_standby.diffs || true
 cp tests/isolation2/regression.diffs /logs/regression_isolation_with_standby.diffs || true
 
-if ! gpinitstandby -a -s $(hostname) -P $((PGPORT + 1)) -S /home/gpadmin/gpdb_src/gpAux/gpdemo/datadirs/standby; then
-  echo "failed to enable standby"
-  exit 1
-fi
+gpinitstandby -a -s $(hostname) -P $((PGPORT + 1)) -S /home/gpadmin/gpdb_src/gpAux/gpdemo/datadirs/standby
 
 # test with standby
-set +e
-cmake --build . --target installcheck
-errors=$(( errors + $? ))
-set -e
+cmake --build . --target installcheck || errors=$(( errors + $? ))
 
 cp tests/regress/regression.diffs /logs/regression_regress_without_standby.diffs || true
 cp tests/isolation2/regression.diffs /logs/regression_isolation_without_standby.diffs || true
