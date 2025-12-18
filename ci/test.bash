688bfd693f5d25b6994a8a1d6041a4fd5cef09d3
Viktor Kurilko <v.kurilko@arenadata.io>
2025-12-18 16:53:48 +0700

    fix

--- ci/test.bash
+++ ci/test.bash
@@ -12,16 +12,25 @@ errors=0
 
 cmake --build . --target installcheck || errors=$(( errors + $? ))
 
+tar -czf "/logs/result.tar.gz" \
+  "tests/regress/results" \
+  "tests/isolation2/results"
+
 cp tests/regress/regression.diffs /logs/regression_regress_with_standby.diffs || true
 cp tests/isolation2/regression.diffs /logs/regression_isolation_with_standby.diffs || true
 
 gpstop -may -M immediate
 export MASTER_DATA_DIRECTORY=/home/gpadmin/gpdb_src/gpAux/gpdemo/datadirs/standby
+export COORDINATOR_DATA_DIRECTORY=$MASTER_DATA_DIRECTORY
 export PGPORT=$(( PGPORT + 1 ))
 gpactivatestandby -a -f -d $MASTER_DATA_DIRECTORY
 
 # Run test again with standby master
 cmake --build . --target installcheck || errors=$(( errors + $? ))
+
+tar -czf "/logs/result_standby.tar.gz" \
+  "tests/regress/results" \
+  "tests/isolation2/results"
 
 cp tests/regress/regression.diffs /logs/regression_regress_without_standby.diffs || true
 cp tests/isolation2/regression.diffs /logs/regression_isolation_without_standby.diffs || true
