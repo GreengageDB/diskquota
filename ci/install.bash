5536ec88971ca01d2f46fed0e6784ad23049c9dc
Viktor Kurilko <v.kurilko@arenadata.io>
2025-12-18 12:38:21 +0700

    fix

--- ci/install.bash
+++ ci/install.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-source /home/gpadmin/gpdb_src/concourse/scripts/common.bash
+cd /home/gpadmin
+source gpdb_src/concourse/scripts/common.bash
 install_and_configure_gpdb
-/home/gpadmin/gpdb_src/concourse/scripts/setup_gpadmin_user.bash
+gpdb_src/concourse/scripts/setup_gpadmin_user.bash
 make_cluster
 mkdir -p /logs
 chown gpadmin:gpadmin /logs
